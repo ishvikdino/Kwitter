6e0d70af56449429f92acf68dd71cc9ef120153e
ishvikdino <84208635+ishvikdino@users.noreply.github.com>
2022-09-25 15:00:52 -0400

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,11 @@
+function send()
+{
+    msg=document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({
+name:user_name;
+message:msg;
+like:0;
+
+    });
+    document.getElementById("msg").value="";
+}
